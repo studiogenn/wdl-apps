55e4bf6dd08a35c568447bc266547a88d48d6f30
Matthew Heim <matt@wfogen.com>
2026-04-02 18:03:09 -0400

    chore: add use client directive to QuizFlow

--- apps/web/src/components/pricing/QuizFlow.tsx
+++ apps/web/src/components/pricing/QuizFlow.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useCallback } from "react";
 import { questions, getSuggestion, type QuizAnswers, type Suggestion } from "./pricing-data";
 import { AddressInput } from "@/components/account/address-input";
